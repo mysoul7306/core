706c9e85ce952a3e9fc3f410a9cbe2d984e4a658
rok_root <mysoul7306@outlook.com>
2021-07-24 17:52:16 +0900

    Seperated module  - Core project added

---
+++ src/main/java/kr/co/rokroot/core/annotations/MariaDB.java
@@ -0,0 +1,20 @@
+/*
+ * Author: rok_root
+ * Created time: 2021. 07. 24
+ * Copyrights rok_root. All rights reserved.
+ */
+
+package kr.co.rokroot.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MariaDB {
+
+
+
+}
